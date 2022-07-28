0c73c7d728c1b89ee639818769ac3a59049f630c
jespinoza12 <jespinoza@student.neumont.edu>
2022-07-28 12:48:41 -0600

    Login and resigester

---
+++ DrinkHub-Frontend/src/components/register/register.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react"
+import "./register.css"
+import axios from "axios"
+import { useHistory } from "react-router-dom"
+
+const Register = () => {
+
+    const history = useHistory()
+
+    const [ user, setUser] = useState({
+        name: "",
+        email:"",
+        username:"",
+        password:"",
+        reEnterPassword: "",
+        age:""
+    })
+
+    const handleChange = e => {
+        const { name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+    const register = () => {
+        const { name, email, password, reEnterPassword, age, username } = user
+        if( username && age && name && email && password && (password === reEnterPassword)){
+            axios.post("http://localhost:9002/register", user)
+            .then( res => {
+                alert(res.data.message)
+                history.push("/login")
+            })
+        } else {
+            alert("invlid input")
+        }
+    }
+
+    return (
+        <div className="register">
+            {console.log("User", user)}
+            <h1>Register</h1>
+            <input type="name" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input>
+            <input type="email" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
+            <input type="username" name="username" value={user.username} placeholder="Username" onChange={ handleChange }></input>
+            <input type="age" name="age" value={user.age} placeholder="Age" onChange={ handleChange }></input>
+            <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
+            <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></input>
+            <div className="button" onClick={register} >Register</div>
+            <div>or</div>
+            <div className="button" onClick={() => history.push("/login")}>Login</div>
+        </div>
+    )
+}
+
+export default Register
