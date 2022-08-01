b1d124858f8b2b9131f9544f4123a498dd49f73a
jsingvah <79827219+jsingvah@users.noreply.github.com>
2022-08-01 10:16:16 -0600

    Homepage CSS

--- DrinkHub-Frontend/src/components/homepage/homepage.js
+++ DrinkHub-Frontend/src/components/homepage/homepage.js
@@ -4,7 +4,20 @@ import "./homepage.css"
 const Homepage = ({setLoginUser}) => {
     return (
         <div className="homepage">
-            <h1>Hello Homepage</h1>
+            <div className="container">
+                <h1>Welcome Customer</h1>
+                <h3>What would you like to order?</h3>
+                <div className="menu">
+                    <div id="red" className="option"></div>
+                    <div id="yellow" className="option"></div>
+                    <div id="blue" className="option"></div>
+                </div>
+                <div className="menu">
+                    <div id="orange" className="option"></div>
+                    <div id="green" className="option"></div>
+                    <div id="purple" className="option"></div>
+                </div>
+            </div>
             <div className="button" onClick={() => setLoginUser({})} >Logout</div>
         </div>
     )
